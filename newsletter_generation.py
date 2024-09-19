5e9fb214fbdad7b33cbb167cbfd06d6cb70634de
shuaibahmed01 <121910026+shuaibahmed01@users.noreply.github.com>
2024-09-19 11:58:18 -0700

    changes to date formatting

--- newsletter_generation.py
+++ newsletter_generation.py
@@ -2,6 +2,10 @@ from jinja2 import Environment, FileSystemLoader
 from datetime import datetime, timedelta
 from collections import defaultdict
 
+def format_date(date_string):
+    date_obj = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+    return date_obj.strftime('%B %d, %Y')
+
 def generate_newsletter(articles):
     # Set up Jinja2 environment
     env = Environment(loader=FileSystemLoader('.'))
@@ -16,9 +20,10 @@ def generate_newsletter(articles):
     # Format the date range
     formatted_date_range = f"{start_of_week.strftime('%B %d')} - {current_date.strftime('%B %d, %Y')}"
 
-    # Group articles by topic
+    # Group articles by topic and format publish dates
     articles_by_topic = defaultdict(list)
     for article in articles:
+        article['formatted_publish_date'] = format_date(str(article['publish_date']))
         articles_by_topic[article['topic']].append(article)
 
     # Render the template with the grouped articles data
